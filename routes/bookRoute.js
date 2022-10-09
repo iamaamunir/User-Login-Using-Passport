a592ef5e33fbf5b8d3eb46d0240fdcff1046ffc9
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-10-09 21:00:55 +0000

    fix:get route

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,6 +3,6 @@ const bookRouter = express.Router()
 const bookController = require('../controllers/bookController')
 
 bookRouter.route('/createbook').post(bookController.createBook)
-bookRouter.route('/books').get(bookController.allBooks)
+bookRouter.route('/').get(bookController.allBooks)
 
 module.exports = bookRouter
