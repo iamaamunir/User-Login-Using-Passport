f3779ab8194d758d41a5b6531a9a0f6783aeb6ca
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-10-17 14:10:55 +0000

    feat:change password router

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,6 +8,8 @@ userRouter.post('/register', userController.signUp)
 userRouter.get('/register', userController.signUpExUser)
 userRouter.post('/logout', userController.logOut)
 userRouter.get('/login', userController.loginExUser)
+userRouter.get('/changepassword', userController.changePasswordPage)
+userRouter.post('/changepassword', userController.changePassword)
 userRouter.post('/login', passport.authenticate('local', { failureRedirect: '/' }), userController.loginUser)
 
 
